8180a65ffe3f938112a8893a16aa38de9de61325
Dipika Choudhary <dipikachoudhary3104@gmail.com>
2023-03-20 12:25:07 +0530

    alertView

--- Alert View/ViewController.swift
+++ Alert View/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveButton(_ sender: UIButton) {
+        showAlert()
+    }
+    
+    func showAlert()
+    {
+        let alert = UIAlertController.init(title: "Save", message: "Save your details.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction.init(title: "Save", style: .default, handler: nil))
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .destructive))
+        present(alert, animated: true)
+    }
 }
 
